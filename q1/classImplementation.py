eefc96da3de02dc465ee6200e0695baed3a42903
xkesta-glitch <xkesta@brc.pshs.edu.ph>
2026-09-09 00:26:58 +0800

    Added new lines of code to classImplementation.py

--- q1/classImplementation.py
+++ q1/classImplementation.py
@@ -22,14 +22,21 @@ class Ppop:
 
 if __name__ == "__main__":
     song = Ppop("Lifetime", "Ben&Ben", "", 277, "06-04-2020")
+    song2 = Ppop("Kalapastangan", "fitterkarma", "", 276, "11-24-2023")
+    print("--- BEFORE ---") 
     song.play()
     print(song.pause())
     print(song.fast_forward(30))
     print(f"Duration: {song.get_duration()} seconds")
     song.set_duration("200")
     print(f"Updated Duration: {song.get_duration()} seconds")
+    song2.play()
+    print(song2.pause())
+    print(song2.fast_forward(50))
+    print(f"Duration: {song2.get_duration()} seconds")
+    song2.set_duration("250")
+    print(f"Updated Duration: {song2.get_duration()} seconds")
 
     print("--- AFTER ---")
     print(f"Object 1: {song.name}, {song.author}, {song.get_duration()} seconds, {song._Ppop__private_release_date}")
-
-    
+    print(f"Object 2: {song2.name}, {song2.author}, {song2.get_duration()} seconds, {song2._Ppop__private_release_date}")
