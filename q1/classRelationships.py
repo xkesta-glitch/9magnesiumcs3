7bea454c4d5d6ad86e5bc93877de37c8a8fa0fc4
xkesta-glitch <xkesta@brc.pshs.edu.ph>
2026-09-24 22:17:50 +0800

    Refactor Ppop class and add Album class

--- q1/classRelationships.py
+++ q1/classRelationships.py
@@ -1,9 +1,8 @@
 class Ppop:
-    def __init__(self, name: str, age: str, author: str, __private_duration: int, __private_release_date: str):
+    def __init__(self, name: str, created: str):
         self.name = name
-        self.author = author
-        self.__private_duration = __private_duration
-        self.__private_release_date = __private_release_date
+        self.created = created
+        self.Ppop_list =[]
     def play(self) -> None:
         print(f"Playing: {self.name} by {self.author}")
     def pause(self) -> None:
@@ -20,6 +19,42 @@ class Ppop:
             print("Invalid duration. Please provide a valid duration.")
 
 if __name__ == "__main__":
+    song = Ppop("Lifetime", "Ben&Ben", "", 277, "06-04-2020")
+    song2 = Ppop("Kalapastangan", "fitterkarma", "", 276, "11-24-2023")
+    print("--- BEFORE ---") 
+    song.play()
+    print(song.pause())
+    print(song.fast_forward(30))
+    print(f"Duration: {song.get_duration()} seconds")
+    song.set_duration("200")
+    print(f"Updated Duration: {song.get_duration()} seconds")
+    song2.play()
+    print(song2.pause())
+    print(song2.fast_forward(50))
+    print(f"Duration: {song2.get_duration()} seconds")
+    song2.set_duration("250")
+    print(f"Updated Duration: {song2.get_duration()} seconds")
+
+    print("--- AFTER ---")
+    print(f"Object 1: {song.name}, {song.author}, {song.get_duration()} seconds, {song._Ppop__private_release_date}")
+    print(f"Object 2: {song2.name}, {song2.author}, {song2.get_duration()} seconds, {song2._Ppop__private_release_date}")
+
+#New Class
+class Album:
+    def __init__(self, name: str, created: str):
+        self.name = name
+        self.created = created
+        self.ppop_list =[]
+        def add_pop(self, ppop: Ppop) -> None:
+        self.ppop_list.append(song)
+        print(f"Added '{ppop.title}' to '{self.name}' album.")
+    def display_album(self) ->None:
+        print(f"Folder: {self.name} (Created: {self.created})")
+        if not self.pop_list:
+            print("Album is empty.")
+            return
+
+if __name__ == "__main__"
     song = Ppop("Lifetime", "Ben&Ben", "", 277, "06-04-2020")
     song2 = Ppop("Kalapastangan", "fitterkarma", "", 276, "11-24-2023")
     print("--- BEFORE ---") 
